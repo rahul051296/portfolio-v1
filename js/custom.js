883e1757c43afd30b9482e75d69e5baa3a7bb0c8
Rahul <rahulprabhakar0596@gmail.com>
2017-11-03 13:16:12 +0530

    Contact Me API

--- js/custom.js
+++ js/custom.js
@@ -2,40 +2,36 @@
     Scroll on Click
 ----------------------*/
 
-$(document).ready(function()
-    {
-        $("a").on('click', function(event)
-            {
-                if (this.hash !== "")
-                    {
-                        event.preventDefault();
-                        var hash = this.hash;
-        $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function()
-                        {
-                        window.location.hash = hash;
-                        });
-                    } 
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
             });
+        }
     });
+});
 
-$(function(){
-    $('#pointer').hover(function(){
+$(function () {
+    $('#pointer').hover(function () {
         $(this).addClass('animated pulse infinite');
-    }).mouseout(function(){ 
+    }).mouseout(function () {
         $(this).removeClass('animated pulse infinite');
     });
-    });
+});
 
- window.onload = function() {
-  Particles.init({
-    selector: '#myCanvas',
-    color: '#00d19c',
-      speed:1.5,
-    
-      
-  });
+window.onload = function () {
+    Particles.init({
+        selector: '#myCanvas',
+        color: '#00d19c',
+        speed: 1.5,
+
+
+    });
 };
 
 
@@ -43,38 +39,52 @@ $(function(){
     Preloader
 ------------------ */
 
-$(window).on('load', function()
-    {
-       
-     $('#preloader').delay(3000).fadeOut();
-    
-    }); 
+$(window).on('load', function () {
 
-$( "#departure" ).dateDropper();
+    $('#preloader').delay(3000).fadeOut();
 
-$(document).ready(function(){
-    
+});
+
+$("#departure").dateDropper();
+
+$(document).ready(function () {
+
     $("#pointer").fadeIn(7000);
 });
 
 
-$(document).ready(function()
-    {       
-        $('#scroll').hide();
-        var scroll_start = 0;
-        var startchange = $('#about');
-        var offset = startchange.offset();
-      
-$(document).scroll(function() 
-    { 
+$(document).ready(function () {
+    $('#scroll').hide();
+    var scroll_start = 0;
+    var startchange = $('#about');
+    var offset = startchange.offset();
+
+    $(document).scroll(function () {
         scroll_start = $(this).scrollTop();
-        if(scroll_start > offset.top)
-            {
-                $('#scroll').show();
-             } 
-         else
-             {
-                 $('#scroll').hide();
+        if (scroll_start > offset.top) {
+            $('#scroll').show();
+        } else {
+            $('#scroll').hide();
+        }
+    });
+});
+
+function check() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var messsage = document.getElementById('message').value;
+    $(document).ready(function () {
+        $.ajax({
+            url: "http://theparkingspace.000webhostapp.com/contact.php?name="+name+"&email="+email+"&phone="+phone+"&message="+messsage,
+            success: function (result) {
+                var res = document.getElementById('response');
+                res.innerHTML = "Your request has been sent.";
+                res.className = "text-center label label-success";
+                setTimeout(()=>{
+                    res.style.display = 'none';
+                },4000);
             }
+        });
     });
-    });
+}
